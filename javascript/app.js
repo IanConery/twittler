c2327cb32c148383aeab970b00ee2985449be594
Ian Conery <ianconery@gmail.com>
2015-06-08 18:54:03 -0700

    Added more style/ started user pages

--- javascript/app.js
+++ javascript/app.js
@@ -9,13 +9,18 @@
             for(i = index; i <= currentArraySize; i++){
 
               var tweet = currentTweetArray[i];
-              var $tweet = $('<li></li>');
-              $tweet.append($('<a class="creator tweet" href="#"></a>').text('@' + tweet.user).attr('href', "userHTML/" + tweet.user + ".html")).append($('<div class="content tweet"></div>').text(tweet.message)).append($('<div class="time tweet"></div>').text(tweet.created_at));
-              $tweet.prependTo('.tweets');
+              var $tweet = $('<div class="container tweet">')
+              var $author = $('<a class="creator" href="">@' + tweet.user + '</a>').attr('href', "userHTML/" + tweet.user + ".html");
+              var $content = $('<p class="content"></p>').text(tweet.message);
+              var $time = $('<p class="time"></p></div>').text(tweet.created_at);
+
+              $tweet.append($author).append($content).append($time).append($('<hr>'));
+              $tweet.prependTo('.tweet-post');
             }
               index = currentArraySize;
 
           };
+
 
           populate();
 
@@ -25,3 +30,4 @@
              }
           }, 1000);*/
   });
+
